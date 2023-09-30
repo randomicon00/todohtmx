e494f1c2697c707a0c3c72b087b7d03fe9700c8d
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-09-30 11:02:17 -0400

    edit: update gitignore

--- todohtmx/todo/models.py
+++ todohtmx/todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Todo:
+    task = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    #created = models.DateTimeField(auto_now_add=True)
+    #edited = models.DateTimeField(auto_now=True)

3e658ecf0215fbd19ce6919fb7702c5fd9aa2f48
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-02 10:41:14 -0400

    edit: fresh restart with DRF

--- todohtmx/todo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Todo(models.Model):
-    task = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
-    #created = models.DateTimeField(auto_now_add=True)
-    #edited = models.DateTimeField(auto_now=True)
--- todohtmx/todohtmx/settings.py
+++ todohtmx/todohtmx/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #user apps
+
+    'rest_framework',
+    # user apps
     'todo',
 ]
 

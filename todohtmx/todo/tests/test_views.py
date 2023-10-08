9ca1db5f7d4f9a127c4eb43e4a71ec81405b6775
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-08 15:54:50 -0400

    edit: fix test comments

--- todohtmx/todo/tests/test_views.py
+++ todohtmx/todo/tests/test_views.py
@@ -6,10 +6,12 @@ from todo.models import Todo
 
 class TodoAPITestCase(TestCase):
 
+    # This method is called before each test.
     def setUp(self):
         Todo.objects.create(task="First task to do")
         Todo.objects.create(task="Second task to do")
 
+    # Test method that gets all todos.
     def test_get_todos(self):
         url = reverse('api_todos')
         response = self.client.get(url)
@@ -17,6 +19,7 @@ class TodoAPITestCase(TestCase):
         self.assertIn('First task', str(response.content))
         self.assertIn('Second task', str(response.content))
 
+    # Test method that posts (adds) a single todo.
     def test_post_todo_valid(self):
         url = reverse('api_todos')
         data = {'task': 'Third task to do'}
@@ -24,6 +27,7 @@ class TodoAPITestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('Third task', str(response.content))
 
+    # Test method that posts a single todo with an invalid (empty) task.
     def test_post_todo_invalid(self):
         url = reverse('api_todos')
         data = {'task': ''}

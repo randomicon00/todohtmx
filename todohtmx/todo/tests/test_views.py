5ee7e737118123cc4e93539f112625516962a521
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-09 07:34:53 -0400

    edit: change the text of the assertIn functions

--- todohtmx/todo/tests/test_views.py
+++ todohtmx/todo/tests/test_views.py
@@ -16,8 +16,8 @@ class TodoAPITestCase(TestCase):
         url = reverse('api_todos')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('First task', str(response.content))
-        self.assertIn('Second task', str(response.content))
+        self.assertIn('First task to do', str(response.content))
+        self.assertIn('Second task to do', str(response.content))
 
     # Test method that posts (adds) a single todo.
     def test_post_todo_valid(self):
@@ -25,7 +25,7 @@ class TodoAPITestCase(TestCase):
         data = {'task': 'Third task to do'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('Third task', str(response.content))
+        self.assertIn('Third task to do', str(response.content))
 
     # Test method that posts a single todo with an invalid (empty) task.
     def test_post_todo_invalid(self):

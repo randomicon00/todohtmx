af7b6de6042b60ecb409a7458f008a1a496f099e
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-24 12:13:38 +0100

    fix: comment out unused code

--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TodoAPIViewOld, TodoAPIView
 
 urlpatterns = [
-    path('apiold/', TodoAPIViewOld.as_view(), name='api_todos_old'),
+    # path('apiold/', TodoAPIViewOld.as_view(), name='api_todos_old'),
     path('api/', TodoAPIView.as_view(), name='api_todos'),
 ]

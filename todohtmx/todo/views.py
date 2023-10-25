6b5dbe37f75745b9a357251ba2744c499baa387b
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-25 12:57:01 +0100

    fix: remove unused dependency

--- todohtmx/todo/views.py
+++ todohtmx/todo/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import Todo
 
 

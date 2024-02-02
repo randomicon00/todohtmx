fc7b817545ca235e9b9a5577fcb2281f39fce268
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-02-02 18:36:45 +0100

    feat: add the repr method

--- todohtmx/todo/models.py
+++ todohtmx/todo/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Todo(models.Model):
     task = models.CharField(max_length=255)
 
+    def __repr__(self) -> str:
+        return f"<Todo {self.task}>"
+
     def __str__(self) -> str:
         return self.task

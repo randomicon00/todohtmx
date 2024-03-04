8d7c4ad51a3651066cb7388e1d7368d6bd290531
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-03-04 18:05:23 +0100

    edit: change max str length

--- todohtmx/todo/models.py
+++ todohtmx/todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Todo(models.Model):
-    task = models.CharField(max_length=255)
+    task = models.CharField(max_length=254)
 
     def __repr__(self) -> str:
         return f"<Todo {self.task}>"

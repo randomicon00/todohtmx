96663c6488b191c7101d0416434d8d05217afd0d
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-03-22 09:15:43 +0000

    edit: enhance api tests

--- todohtmx/todo/tests/test_views.py
+++ todohtmx/todo/tests/test_views.py
@@ -12,24 +12,25 @@ class TodoAPITestCase(TestCase):
 
     # Test method that gets all todos.
     def test_get_todos(self):
-        url = reverse('api_todos')
+        url = reverse("api_todos")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('First task to do', str(response.content))
-        self.assertIn('Second task to do', str(response.content))
+        self.assertIn("First task to do", str(response.content))
+        self.assertIn("Second task to do", str(response.content))
+        self.assertIn("Third task to do", str(response.content))
 
     # Test method that posts (adds) a single todo.
     def test_post_todo_valid(self):
-        url = reverse('api_todos')
-        data = {'task': 'Third task to do'}
-        response = self.client.post(url, data, format='json')
+        url = reverse("api_todos")
+        data = {"task": "Fourth task to do"}
+        response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('Third task to do', str(response.content))
+        self.assertIn("Fourth task to do", str(response.content))
 
     # Test method that posts a single todo with an empty task.
-    def test_post_todo_invalid(self):
-        url = reverse('api_todos')
-        data = {'task': ''}
-        response = self.client.post(url, data, format='json')
+    def test_post_todo_empty_invalid(self):
+        url = reverse("api_todos")
+        data = {"task": ""}
+        response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn('Please provide a task', str(response.content))
+        self.assertIn("Please provide a task", str(response.content))

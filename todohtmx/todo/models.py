3ba4ee415093490998cb97a92cc9121d42e1d35d
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-03-23 10:14:55 +0000

    feat: add faq model

--- todohtmx/todo/models.py
+++ todohtmx/todo/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class Faq(models.Model):
+    message = models.CharField(max_length=255)
+
+    def __repr__(self) -> str:
+        return f"< {self.message}>"
+
+    def __str__(self) -> str:
+        return self.message
+
+
 class Todo(models.Model):
     task = models.CharField(max_length=255)
 

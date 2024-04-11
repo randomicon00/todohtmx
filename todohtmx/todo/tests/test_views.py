bca67add386a6d60f389bed077473a7df648a317
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-04-11 11:11:36 -0400

    edit: remove dots from sentences in comments

--- todohtmx/todo/tests/test_views.py
+++ todohtmx/todo/tests/test_views.py
@@ -10,7 +10,7 @@ class TodoAPITestCase(TestCase):
         Todo.objects.create(task="First task to do")
         Todo.objects.create(task="Second task to do")
 
-    # Test method that gets all todos.
+    # Test method that gets all todos
     def test_get_todos(self):
         url = reverse("api_todos")
         response = self.client.get(url)
@@ -19,7 +19,7 @@ class TodoAPITestCase(TestCase):
         self.assertIn("Second task to do", str(response.content))
         self.assertIn("Third task to do", str(response.content))
 
-    # Test method that posts (adds) a single todo.
+    # Test method that posts (adds) a single todo
     def test_post_todo_valid(self):
         url = reverse("api_todos")
         data = {"task": "Fourth task to do"}
@@ -27,7 +27,7 @@ class TodoAPITestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn("Fourth task to do", str(response.content))
 
-    # Test method that posts a single todo with an empty task.
+    # Test method that posts a single todo with an empty task
     def test_post_todo_empty_invalid(self):
         url = reverse("api_todos")
         data = {"task": ""}

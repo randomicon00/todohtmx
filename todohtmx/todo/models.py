d5869f4eae7f6db362c0bd0b2c5ed8db1366966a
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-04-13 12:14:40 -0400

    edit: model repr function

--- todohtmx/todo/models.py
+++ todohtmx/todo/models.py
@@ -5,7 +5,7 @@ class Todo(models.Model):
     task = models.CharField(max_length=255)
 
     def __repr__(self) -> str:
-        return f"<Todo {self.task}>"
+        return f"<Todo: {self.task}>"
 
     def __str__(self) -> str:
         return self.task

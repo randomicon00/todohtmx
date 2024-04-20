2939ea6cbe0f8ae8805324a200765f58c585bae9
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-04-20 13:01:51 -0400

    edit: add a relevant comment

--- todohtmx/todohtmx/urls.py
+++ todohtmx/todohtmx/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('todo/', include('todo.urls')),
+    path("admin/", admin.site.urls),
+    path("todo/", include("todo.urls")),
+    # add more paths in here
 ]

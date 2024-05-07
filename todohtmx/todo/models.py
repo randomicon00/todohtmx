b3995cb6e1ae7d5bddd76b3e6b167b3e3aa58cbe
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-05-07 08:18:52 -0400

    feat: implement the statistics model

--- todohtmx/todo/models.py
+++ todohtmx/todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Count
 
 
 class Todo(models.Model):
@@ -34,3 +35,21 @@ class Faq(model.Models):
 
 
 # TODO Statistics Model
+
+
+class Statistics(models.Model):
+    pending_count = models.IntegerField(default=0)
+    in_progress_count = models.IntegerField(default=0)
+    completed_count = models.IntegerField(default=0)
+
+    def update_statistics(self):
+        self.pending_count = Todo.objects.filter(status=1).count()
+        self.in_progress_count = Todo.objects.filter(status=2).count()
+        self.completed_count = Todo.objects.filter(status=3).count()
+        self.save()
+
+    def __repr__(self) -> str:
+        return f"<Statistics Pending: {self.pending_count}, In Progress: {self.in_progress_count}, Completed: {self.completed_count}>"
+
+    def __str__(self) -> str:
+        return f"Pending: {self.pending_count}, In Progress: {self.in_progress_count}, Completed: {self.completed_count}"

068f74d6231426c7f84c9cf061280571a40214d3
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-05-08 08:47:31 -0400

    feat: implement Statistics Serializer

--- todohtmx/todo/serializers.py
+++ todohtmx/todo/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import Serializers
-from .models import Faq
+from .models import Faq, Statistics
 
 
 class FaqSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Faq
-        fields = ["id", "question", "answer"]
+        fields = ["question", "answer"]
+
+
+class StatisticsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Statistics
+        fields = ["pending_count", "in_progress_count", "completed_count"]

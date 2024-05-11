fb9785e8d22b4bea1a3a3c02c182e8d9cbf35d73
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-05-11 08:34:47 -0400

    edit: add Statistics model to admin

--- todohtmx/todo/admin.py
+++ todohtmx/todo/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Todo, Faq
+from .models import Todo, Faq, Statistics
 
 admin.site.register(Todo)
 admin.site.register(Faq)
+admin.site.register(Statistics)

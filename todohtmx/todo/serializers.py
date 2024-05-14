42c1747f88503b2f4b19278d41daaf13ab648090
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-05-14 08:57:48 -0400

    feat: add serializer helper functions

--- todohtmx/todo/serializers.py
+++ todohtmx/todo/serializers.py
@@ -9,8 +9,23 @@ class FaqSerializer(serializers.ModelSerializer):
         fields = ["question", "answer"]
 
 
+    def get_question_length(self, obj):
+        return len(obj.question)
+
+    def validate_question(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError("The question must be at least 10 characters length"
+        return value
+
 class StatisticsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Statistics
         fields = ["pending_count", "in_progress_count", "completed_count"]
+
+    def get_total_count(self, obj):
+        return obj.pending_count + obj.progress_count + obj.completed_count
+
+    def get_completed_percentage(self, obj):
+        total = self.get_total_count(obj)
+        return (obj.completed_count / total * 100) if total > 0 else 0

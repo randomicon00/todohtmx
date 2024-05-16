b2f6fa962f7d6ff3814f813679c724eb42ca89a6
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-05-16 07:58:03 -0400

    edit: clean up comment

--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("api/", TodoAPIView.as_view(), name="todos_api"),
     path("api/faqs", FaqAPIView.as_view(), name="faq_api"),
     path("api/stats", StatisticsAPIView.as_view(), name="stats_api"),
-    # add more app paths here
 ]

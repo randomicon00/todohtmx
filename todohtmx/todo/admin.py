3dd9302ca1376c1621c789292fc83ebea9e497ea
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-05-30 08:40:47 -0400

    edit: add admin classes

--- todohtmx/todo/admin.py
+++ todohtmx/todo/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Todo, Faq, Statistics
 
+
+class TodoAdmin(admin.ModelAdmin):
+    pass
+
+
+class FaqAdmin(admin.ModelAdmin):
+    pass
+
+
+class StatisticsAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Todo)
 admin.site.register(Faq)
 admin.site.register(Statistics)

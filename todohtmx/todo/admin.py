54a33497074a5459a335c8428373be1c683cbf9e
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-05-31 08:48:31 -0400

    edit: enhance admin model display

--- todohtmx/todo/admin.py
+++ todohtmx/todo/admin.py
@@ -3,15 +3,28 @@ from .models import Todo, Faq, Statistics
 
 
 class TodoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("task", "status", "status_display")
+    list_filter = ("status",)
+    search_fields = ("task",)
+
+    def status_display(self, obj):
+        return obj.get_status_display()
+
+    status_display.short_description = "Status"
 
 
 class FaqAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("task", "status", "status_display")
+    search_fields = ("question",)
 
 
 class StatisticsAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "pending_count",
+        "in_progress_count",
+        "completed_count",
+        "archived_count",
+    )
 
 
 admin.site.register(Todo)

ad05a87e2df9e4eaf3a862d590fdbccd09c61c95
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-06-04 07:58:06 -0400

    fix: small typo

--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -3,6 +3,6 @@ from .views import TodoAPIView, FaqAPIView, StatisticsAPIView
 
 urlpatterns = [
     path("api/", TodoAPIView.as_view(), name="todos_api"),
-    path("api/faqs", FaqAPIView.as_view(), name="faq_api"),
+    path("api/faq", FaqAPIView.as_view(), name="faq_api"),
     path("api/stats", StatisticsAPIView.as_view(), name="stats_api"),
 ]

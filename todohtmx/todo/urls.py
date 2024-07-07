57f0f168c68bdd95e8464fbab0f2b7194852cbe7
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-07-07 10:39:07 -0400

    edit: add chat path

--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path("api/stats", StatisticsAPIView.as_view(), name="stats_api"),
     # TODO use web sockets and htmx to build a live chat support app
     # TODO Add consumer and middleware logic
-    # path("api/chat", consumers.ChatConsumer.as_asgi(), name="stats_api"),
-    # path("api/chat", ChatAPIView.as_view(), name="chat_api"),
-    # add more paths here
+    path("api/chat", consumers.ChatConsumer.as_asgi(), name="stats_api"),
 ]

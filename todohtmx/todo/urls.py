2dfc2f402f2769186eab10ff32b9c6a10813a85e
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-07-24 08:43:10 -0400

    fix: chat url naming

--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("api/stats", StatisticsAPIView.as_view(), name="stats_api"),
     # TODO use web sockets and htmx to build a live chat support app
     # TODO Add consumer and middleware logic
-    path("api/chat", consumers.ChatConsumer.as_asgi(), name="stats_api"),
+    path("api/chat", consumers.ChatConsumer.as_asgi(), name="chat_api"),
 ]

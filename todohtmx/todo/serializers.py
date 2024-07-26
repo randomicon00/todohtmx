add48feecdb0fdedd6e9c0aab03640d80cafccf9
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-07-26 09:01:02 -0400

    edit: modify serializer array of fields

--- todohtmx/todo/serializers.py
+++ todohtmx/todo/serializers.py
@@ -40,4 +40,4 @@ class StatisticsSerializer(serializers.ModelSerializer):
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ["content", "timestamp"]
+        fields = ["timestamp", "content"]

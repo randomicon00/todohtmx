b037f2b5ed1d23ade78a02b9e749a4f248e10de4
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-07-30 08:56:19 -0400

    edit: implement chat consumer connect

--- todohtmx/todo/consumers.py
+++ todohtmx/todo/consumers.py
@@ -5,9 +5,14 @@ from channels.generic.websocket import WebsocketConsumer
 # TODO complete the implementation
 class ChatConsumer(WebSocketConsumer):
     def connect(self):
-        pass
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+
+        self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        self.accept()
 
     def disconnect(self):
+        # TODO leave room group
         pass
 
     def receive(self, text_data):

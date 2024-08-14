83c843914fda0f5daaf31c90941d7cbdc06f5c56
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-08-14 09:15:50 -0400

    fix: fix chat consumer class

--- todohtmx/todo/consumers.py
+++ todohtmx/todo/consumers.py
@@ -1,5 +1,6 @@
-import json
 from channels.generic.websocket import WebsocketConsumer
+import json
+from asgiref.sync import async_to_sync
 
 
 # TODO complete the implementation
@@ -8,19 +9,24 @@ class ChatConsumer(WebSocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self):
         # Leave room group
-        self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     def receive(self, text_data):
         data = json.loads(text_data)
         message = data["message"]
         sender = data["sender"]
 
-        self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             {
                 "type": "chat_message",

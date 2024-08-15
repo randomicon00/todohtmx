0fed4d323e827ad95eb8acf868267a53d11a2ab4
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-08-15 08:28:16 -0400

    feat: add websocket support

--- todohtmx/todohtmx/settings.py
+++ todohtmx/todohtmx/settings.py
@@ -187,3 +187,20 @@ LOGGING = {
 }
 
 SOME_SETTINGS = 10
+
+
+# To support websockets for django apps
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from todo.routing import websocket_urlpatterns
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todohtmx.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)

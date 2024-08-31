02fab4980ab0461d2ccf62efe5011edd9865c094
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-08-31 08:58:21 -0400

    fix: resolve unresolved imports

--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
 from .views import TodoAPIView, FaqAPIView, StatisticsAPIView
+from .consumers import ChatConsumer
 
 urlpatterns = [
     path("api/", TodoAPIView.as_view(), name="todos_api"),
     path("api/faq", FaqAPIView.as_view(), name="faq_api"),
     path("api/stats", StatisticsAPIView.as_view(), name="stats_api"),
     # TODO use web sockets and htmx to build a live chat support app
-    # TODO Add consumer and middleware logic
-    path("api/chat", consumers.ChatConsumer.as_asgi(), name="chat_api"),
+    path("api/chat", ChatConsumer.as_asgi(), name="chat_api"),
 ]
 
 websocket_urlpatters = [
     path(
         "ws/<str:room_name>/",
-        consumers.ChatConsumer.as_asgi(),
+        ChatConsumer.as_asgi(),
         name="experimental_chat_api",
     ),
 ]

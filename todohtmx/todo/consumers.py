7991cbb83f816f9940943380af412df0b320c66f
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-09-03 09:03:10 -0400

    fix: change consumer for one to one chat

--- todohtmx/todo/consumers.py
+++ todohtmx/todo/consumers.py
@@ -2,12 +2,25 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 import json
 
-
 class ChatConsumer(WebsocketConsumer):
+    clients = {}
+
     def connect(self):
-        # Connect to a chat room identified by an url
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
+        client_type = self.scope["url_route"]["kwargs"]["client_type"]
+
+        # Restrict to one agent and one customer
+        if client_type == "agent":
+            if "agent" in ChatConsumer.clients:
+                self.close()
+                return
+            ChatConsumer.clients["agent"] = self.channel_name
+        elif client_type == "customer":
+            if "customer" in ChatConsumer.clients:
+                self.close()
+                return
+            ChatConsumer.clients["customer"] = self.channel_name
 
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
@@ -15,19 +28,22 @@ class ChatConsumer(WebsocketConsumer):
 
         self.accept()
 
-    def disconnect(self):
-        # Leave room group
+    def disconnect(self, close_code):
+        # Remove from clients
+        client_type = self.scope["url_route"]["kwargs"]["client_type"]
+        if client_type in ChatConsumer.clients:
+            del ChatConsumer.clients[client_type]
+
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
 
     def receive(self, text_data):
-        # Receive a message
         data = json.loads(text_data)
         message = data["message"]
         sender = data["sender"]
 
-        async_to_sync(self.channel_layer.group_add)(
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 "type": "chat_message",
@@ -36,8 +52,7 @@ class ChatConsumer(WebsocketConsumer):
             },
         )
 
-    def chat_messsage(self, event):
-        # Send a message
+    def chat_message(self, event):
         message = event["message"]
         sender = event["sender"]
 

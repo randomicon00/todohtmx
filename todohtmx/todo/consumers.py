f9fd57488f833c6892069c6cb7abc688694ade7a
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-09-09 09:02:10 -0400

    refactor: added helper functions to chat consumer

--- todohtmx/todo/consumers.py
+++ todohtmx/todo/consumers.py
@@ -10,57 +10,45 @@ class ChatConsumer(WebsocketConsumer):
         self.room_group_name = f"chat_{self.room_name}"
         client_type = self.scope["url_route"]["kwargs"]["client_type"]
 
-        # Restrict to one agent and one customer for one on one conversation
-        if client_type == "agent":
-            if "agent" in ChatConsumer.clients:
-                self.close()
-                return
-            ChatConsumer.clients["agent"] = self.channel_name
-        elif client_type == "customer":
-            if "customer" in ChatConsumer.clients:
-                self.close()
-                return
-            ChatConsumer.clients["customer"] = self.channel_name
+        if not self.add_client(client_type):
+            self.close()
+            return
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
         self.accept()
 
     def disconnect(self, close_code):
-        # Remove from clients
         client_type = self.scope["url_route"]["kwargs"]["client_type"]
-        if client_type in ChatConsumer.clients:
-            del ChatConsumer.clients[client_type]
+        self.remove_client(client_type)
 
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
 
     def receive(self, text_data):
         data = json.loads(text_data)
-        message = data["message"]
-        sender = data["sender"]
+        message = data.get("message")
+        sender = data.get("sender")
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                "type": "chat_message",
-                "message": message,
-                "sender": sender,
-            },
-        )
-
-    def chat_message(self, event):
-        message = event["message"]
-        sender = event["sender"]
-
-        self.send(
-            text_data=json.dumps(
+        if message and sender:
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
                 {
+                    "type": "chat_message",
                     "message": message,
                     "sender": sender,
-                }
+                },
             )
-        )
+
+    def chat_message(self, event):
+        self.send_json({
+            "message": event["message"],
+            "sender": event["sender"],
+        })
+
+    def add_client(self, client_type):
+        if client_type in ChatConsumer.clients:
+            return False
+        ChatConsumer.clients[client_type] = self.channel_name
+        return True
+
+    def remove_client(self, client_type):
+        ChatConsumer.clients.pop(client_type, None)

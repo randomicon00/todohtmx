4b1a88fe42c3e721094eaf7d6a84057ccc648cdc
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-09-12 08:45:27 -0400

    fix: corrected a missing closing parenthesis

--- todohtmx/todo/serializers.py
+++ todohtmx/todo/serializers.py
@@ -8,37 +8,52 @@ class FaqSerializer(serializers.ModelSerializer):
         model = Faq
         fields = ["question", "answer", "question_length"]
 
-
     def get_question_length(self, obj):
         return len(obj.question)
 
     def validate_question(self, value):
         if len(value) < 10:
-            raise serializers.ValidationError("Question must be at least 10 characters length"
+            raise serializers.ValidationError(
+                "Question must be at least 10 characters length"
+            )
         return value
 
     def validate_answer(self, value):
         if len(value) < 10:
-            raise serializers.ValidationError("Answer must be at least 10 characters in length")
+            raise serializers.ValidationError(
+                "Answer must be at least 10 characters in length"
+            )
         return value
+
 
 class StatisticsSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Statistics 
-        fields = ["pending_count", "in_progress_count", "completed_count", "total_count", "archived_count"]
+        model = Statistics
+        fields = [
+            "pending_count",
+            "in_progress_count",
+            "completed_count",
+            "total_count",
+            "archived_count",
+        ]
         read_only_fields = ["total_count", "completed_percentage"]
 
     def get_total_count(self, obj):
-        return obj.pending_count + obj.progress_count + obj.completed_count + obj.archived_count
+        return (
+            obj.pending_count
+            + obj.progress_count
+            + obj.completed_count
+            + obj.archived_count
+        )
 
     def get_completed_percentage(self, obj):
         total_count = self.get_total_count(obj)
         done_count = obj.completed_count + obj.archived_count
         return (done_count / total_count * 100) if total_count > 0 else 0
 
+
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ["content", "timestamp"]
-

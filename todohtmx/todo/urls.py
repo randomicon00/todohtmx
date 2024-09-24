bae37381d93c1f3f8f64840c01c7dffd4b843126
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-09-24 09:20:26 -0400

    Refactor: Add trailing slash for consistency

--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from .views import TodoAPIView, FaqAPIView, StatisticsAPIView
 from .consumers import ChatConsumer
 
+# API URL patterns
 urlpatterns = [
     path("api/", TodoAPIView.as_view(), name="todos_api"),
-    path("api/faq", FaqAPIView.as_view(), name="faq_api"),
-    path("api/stats", StatisticsAPIView.as_view(), name="stats_api"),
-    # TODO use web sockets and htmx to build a live chat support app
-    path("api/chat", ChatConsumer.as_asgi(), name="chat_api"),
+    path("api/faq/", FaqAPIView.as_view(), name="faq_api"),
+    path("api/stats/", StatisticsAPIView.as_view(), name="stats_api"),
+    # WebSocket-based live chat support app
+    path("api/chat/", ChatConsumer.as_asgi(), name="chat_api"),
 ]
 
+# WebSocket URL patterns
 websocket_urlpatterns = [
     path(
         "ws/<str:room_name>/",

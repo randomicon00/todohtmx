d81a062f9906222334ea3961999cf1558babd958
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-10-07 08:25:18 -0400

    edit: remove unneeded comment

--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import TodoAPIView, FaqAPIView, StatisticsAPIView
 from .consumers import ChatConsumer
 
-# API URL patterns
 urlpatterns = [
     path("api/", TodoAPIView.as_view(), name="todos_api"),
     path("api/faq/", FaqAPIView.as_view(), name="faq_api"),

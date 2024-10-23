07a401381b43b64651d0140a772036327f2b2f8e
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-10-23 08:36:24 -0400

    fix: refactor and correct delete todo test

--- todohtmx/todo/tests/test_views.py
+++ todohtmx/todo/tests/test_views.py
@@ -9,7 +9,7 @@ class TodoAPITestCase(TestCase):
     def setUp(self):
         self.todo1 = Todo.objects.create(task="First task to do")
         self.todo2 = Todo.objects.create(task="Second task to do")
-        self.todo_url = reverse("api_todos")
+        self.todo_url = reverse("todos_api")
 
     def assertTaskInResponse(self, task, response_content):
         """Helper method to check if a task is in the response content."""
@@ -36,15 +36,15 @@ class TodoAPITestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("Please provide a task", str(response.content))
 
-    def test_delete_todo_by_id(self):
-        """Test that a DELETE request deletes a todo by its ID."""
-        data = {"task": "Task to delete"}
-        create_response = self.client.post(self.todo_url, data, format="json")
-        todo_id = create_response.data["id"]
+    def test_delete_todo(self):
+        """Test that a DELETE request deletes a todo using the list endpoint."""
+        todo_to_delete = Todo.objects.create(task="Task to delete")
+        delete_url = reverse("todos_api") + f"/{todo_to_delete.id}/"
 
-        delete_url = reverse("api_todo_detail", args=[todo_id])
+        # Deleting the todo by ID
         delete_response = self.client.delete(delete_url, format="json")
         self.assertEqual(delete_response.status_code, status.HTTP_204_NO_CONTENT)
 
+        # Verifying the todo is deleted
         get_response = self.client.get(delete_url, format="json")
         self.assertEqual(get_response.status_code, status.HTTP_404_NOT_FOUND)
--- todohtmx/todo/urls.py
+++ todohtmx/todo/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from .views import TodoAPIView, FaqAPIView, StatisticsAPIView
-from .consumers import ChatConsumer
 
 urlpatterns = [
-    path("api/", TodoAPIView.as_view(), name="todos_api"),
+    path("api/todos", TodoAPIView.as_view(), name="todos_api"),
     path("api/faq/", FaqAPIView.as_view(), name="faq_api"),
     path("api/stats/", StatisticsAPIView.as_view(), name="stats_api"),
 ]
